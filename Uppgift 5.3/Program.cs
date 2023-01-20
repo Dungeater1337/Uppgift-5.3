3ef579211997a6991343800bd8c762ee701d74e2
Meysam03 <simon.munter@icloud.com>
2023-01-20 11:07:32 +0100

    Add project files.

---
+++ Uppgift 5.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] frågor = { "Vad är det högsta berget i världen?", "Vad är det största landet i världen?", "Vad är den längsta floden i världen?", "Vilken planet är närmast solen?" };
+        string[] svar = { "Mount Everest", "Ryssland", "Nilen", "Mercury" };
+
+        
+        Console.WriteLine("Välj en fråga genom att ange ett tal från 1 till 4:");
+        int userChoice = int.Parse(Console.ReadLine()) - 1;
+
+       
+        Console.WriteLine("Fråga: " + frågor[userChoice]);
+
+     
+        Console.WriteLine("Svara på frågan:");
+        string userAnswer = Console.ReadLine();
+
+   
+        if (userAnswer.ToLower() == svar[userChoice].ToLower())
+        {
+            Console.WriteLine("Rätt svar!");
+        }
+        else
+        {
+            Console.WriteLine("Fel svar. Det rätta svaret är: " + svar[userChoice]);
+        }
+    }
+}
